1cbee099fff6aac332eaef6cdbdf815660468d24
MohamedElnaggar0 <mohamedelnaggarn@gmail.com>
2025-02-20 16:36:02 +0200

    product name and title in shop view done

--- lib/features/home/presentation/views/widgets/shop_view_body.dart
+++ lib/features/home/presentation/views/widgets/shop_view_body.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:stylish_app/core/utils/app_router.dart';
 import 'package:stylish_app/core/widgets/custom_appbar.dart';
+import 'package:stylish_app/core/widgets/start_rating.dart';
 import 'package:stylish_app/features/home/presentation/views/widgets/product_show.dart';
 import 'package:stylish_app/features/home/presentation/views/widgets/size_widget.dart';
 import 'package:stylish_app/gen/assets.gen.dart';
@@ -15,6 +16,7 @@ class ShopViewBody extends StatelessWidget {
       child: Padding(
         padding: const EdgeInsets.all(10.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             const SizedBox(height: 30),
             CustomAppbar(
@@ -25,6 +27,27 @@ class ShopViewBody extends StatelessWidget {
             ),
             const ProductShow(),
             const SizeWidget(),
+            const SizedBox(height: 10),
+            const Text(
+              'Nike Sneakers',
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            ),
+            const Text(
+              'Vision Alta Men’s Shoes Size (All Colours)',
+              style: TextStyle(fontSize: 15),
+            ),
+            const Row(
+              children: [
+                StarRating(),
+                SizedBox(
+                  width: 8,
+                ),
+                Text(
+                  '56,890',
+                  style: TextStyle(color: Colors.grey),
+                )
+              ],
+            ),
           ],
         ),
       ),
